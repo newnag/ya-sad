d5a90ee37d4a956e3e130395a46a7bf7e0bd712e
newnag <48855584+newnag@users.noreply.github.com>
2021-01-06 17:26:38 +0700

    06/01/2021

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function open_mobile_menu(){
+  document.querySelector('nav').classList.add('show')
+}
+
+function close_mobile_menu(){
+  document.querySelector('nav').classList.remove('show')
+}
+
+function open_menu_account(){
+  document.querySelector('.menu-account').classList.toggle('show')
+}
