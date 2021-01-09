ba0d347b7ea5f4d14c8493ec0ea83a42d830cc58
newnag <48855584+newnag@users.noreply.github.com>
2021-01-09 17:15:36 +0700

    09/01/2021

--- js/home.js
+++ js/home.js
@@ -10,3 +10,32 @@ const slide_home = new Swiper('.head-slide', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+///////////////////////////////////////////////////////////////////////
+// สไลด์ของเซลล์
+const slide_sale = new Swiper('.grid-sale', {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2000,
+  },
+  breakpoints:{
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+  }
+});
